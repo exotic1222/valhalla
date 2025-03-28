de96c863f4721e083e025b9a18ed94bdc97f2b92
David <quickscope018@gmail.com>
2025-03-28 07:01:57 -0500

    Corrección de settings y eliminación de credenciales

--- Proyecto_SENA/urls.py
+++ Proyecto_SENA/urls.py
@@ -1,31 +1,40 @@
-"""
-URL configuration for Proyecto_SENA project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from vahalla import views  # Importar las vistas desde la app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('vahalla.urls')),  # Incluye las URLs de tu app correctamente
+    path('', views.index, name='index'),
+    path('menu/', views.menu, name='menu'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('perfil/', views.perfil, name='perfil'),
+    path('eventos/', views.eventos, name='eventos'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('logout/', views.logout_view, name='logout'),
+    path('reservas/', views.reservas, name='reservas'),
+    path('reservar/', views.reservar, name='reservar'),
+    path('manual_usuario/', views.manual_usuario, name='manual_usuario'),
+    path('add_to_cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove_from_cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    
+    # Rutas de recuperación de contraseña
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+
+    # Otras funciones de la app
+    path('send_reclaim_email/', views.send_reclaim_email, name='send_reclaim_email'),
+    path('send_domicilio_email/', views.send_domicilio_email, name='send_domicilio_email'),
+    path('redirect_to_whatsapp/', views.redirect_to_whatsapp, name='redirect_to_whatsapp'),
+    path('clear_cart/', views.clear_cart, name='clear_cart'),
 ]
 
-# Solo servir archivos estáticos y media en modo DEBUG
+# Configuración para servir archivos estáticos y media en modo DEBUG
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
